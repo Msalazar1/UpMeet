ab5383fdd65bd06c45a3c577461433bbb1725668
Manny <mannysalazar@quickenloans.com>
2020-10-19 15:47:05 -0400

    Add project files.

---
+++ UpMeet/Models/Favorites.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UpMeet.Models
+{
+    public class Favorites
+    {
+        public string Username { get; set; }
+        public long EventID { get; set; }
+    }
+}
